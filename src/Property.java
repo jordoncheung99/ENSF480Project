0262a5d2169067cfff8e343e80c2a4833b72a74a
jordoncheung99 <jordoncheung99@gmail.com>
2019-11-18 12:15:18 -0700

    Setup JDK I think?

---
+++ src/Property.java
@@ -0,0 +1,12 @@
+public class Property {
+    private float rentAmmount;
+    private float rentTerm;
+    private float area;
+    private String typeOfProperty;
+
+
+
+    public static void main(String arg[]){
+
+    }
+}
