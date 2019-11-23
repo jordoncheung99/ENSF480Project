24a8247fd56fefd20ba39c14d30c5e041d2d9f19
Derek Braun <derek.braun.99@gmail.com>
2019-11-23 14:58:45 -0700

    Complete login with database

--- src/LoginServer.java
+++ src/LoginServer.java
@@ -15,7 +15,6 @@ public class LoginServer {
     public static void main(String args[]){
         LoginServer server = getInstance();
         server.loadDataBase();
-        server.removeUser("Tim","321");
         server.saveDataBase();
     }
 
@@ -32,23 +31,29 @@ public class LoginServer {
     }
 
     public void addUser(String username, String password, int type){
-        //TODO translate to work with database
-
-        //Check if the username exists
-        for(User temp: users){
-            if(temp.username.equals(username)){
-                System.out.println("Username already exists, please pick another name");
-                return;
-            }
-        }
-        //TODO: Translate to work with a database.
-        users.add(new User(username, password, type));
+//        //TODO translate to work with database
+//
+//        //Check if the username exists
+//        for(User temp: users){
+//            if(temp.username.equals(username)){
+//                System.out.println("Username already exists, please pick another name");
+//                return;
+//            }
+//        }
+//        //TODO: Translate to work with a database.
+//        users.add(new User(username, password, type));
+//        try{
+//            Connection conn = database.getConnection();
+//            Statement addUser = conn.createStatement();
+//            String query = "INSERT "
+//        }catch(SQLException e)
+//        {
+//            e.printStackTrace();
+//        }
     }
 
     public User validate(String username, String password){
         try {
-            database = new MySQLDatabase();
-            database.initializeConnection();
             Connection conn = database.getConnection();
             Statement validateUsers = conn.createStatement();
             String query = "SELECT * FROM User WHERE Uname = '" + username + "' AND Pass = '" + password + "'";
@@ -62,7 +67,6 @@ public class LoginServer {
             }
         }catch(SQLException e){
             e.printStackTrace();
-            System.err.println("Error - Could not execute query");
         }
         return null;
     }
@@ -91,16 +95,16 @@ public class LoginServer {
     }
 
     public void saveDataBase(){
-        //TODO: Translate to work with database
-        try {
-            PrintWriter writer = new PrintWriter("Users.txt");
-            for(User temp: users){
-                writer.println(temp.username + " " + temp.password + " " + temp.type);
-            }
-            writer.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+//        //TODO: Translate to work with database
+//        try {
+//            PrintWriter writer = new PrintWriter("Users.txt");
+//            for(User temp: users){
+//                writer.println(temp.username + " " + temp.password + " " + temp.type);
+//            }
+//            writer.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
 
     }
 
