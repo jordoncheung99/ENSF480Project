998593d5b0a37e90bccce5c134b3c1a13d598583
rnward0 <49202674+rnward0@users.noreply.github.com>
2019-11-23 16:26:56 -0700

    Register GUI

---
+++ src/GUI/RegisterGUI.java
@@ -0,0 +1,32 @@
+package GUI;
+
+import javax.swing.*;
+
+public class RegisterGUI {
+    private JPanel panel1;
+    private JPanel leftLabels;
+    private JPanel rightTextFields;
+    private JPanel bottomButtons;
+    private JPanel topTitle;
+    private JLabel firstNameLabel;
+    private JLabel middleNameLabel;
+    private JLabel lastNameLabel;
+    private JLabel streetLabel;
+    private JLabel cityLabel;
+    private JLabel cityQudrantLabel;
+    private JLabel provinceLabel;
+    private JLabel postalCodeLabel;
+    private JLabel countryLabel;
+    private JLabel registerTitle;
+    private JTextField firstNameField;
+    private JTextField middleNameField;
+    private JTextField lastNameField;
+    private JTextField streetField;
+    private JTextField cityField;
+    private JTextField cityQuadrantField;
+    private JTextField provinceField;
+    private JTextField countryField;
+    private JTextField postalCodeField;
+    private JButton registerButton;
+    private JButton backButton;
+}
