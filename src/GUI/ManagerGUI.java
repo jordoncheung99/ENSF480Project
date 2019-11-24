0fa501294f5d8463e64ab6608ff436b08365523b
rnward0 <49202674+rnward0@users.noreply.github.com>
2019-11-23 19:48:54 -0700

    Landlord and manager GUIs

--- src/GUI/ManagerGUI.java
+++ src/GUI/ManagerGUI.java
@@ -1,4 +1,12 @@
 package GUI;
 
+import javax.swing.*;
+
 public class ManagerGUI {
+    private JPanel panel1;
+    private JPanel titlePanel;
+    private JPanel buttonPanel;
+    private JLabel managerLabel;
+    private JButton viewLandlordButton;
+    private JButton requestSummaryButton;
 }
