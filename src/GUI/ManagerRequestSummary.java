28c74142e0fbf83eaf669819ea46d2ba8605df85
rnward0 <49202674+rnward0@users.noreply.github.com>
2019-11-23 21:53:52 -0700

    Rest of the GUIs

---
+++ src/GUI/ManagerRequestSummary.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class ManagerRequestSummary {
+    private JPanel panel1;
+    private JPanel titlePanel;
+    private JPanel buttonPanel;
+    private JButton backButton;
+}
---
+++ src/GUI/RenterSelectPropertyGUI.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.*;
+
+public class RenterSelectPropertyGUI {
+    private JPanel panel1;
+    private JPanel titlePanel;
+    private JPanel contentPanel;
+    private JLabel titleLabel;
+    private JPanel buttonPanel;
+    private JButton emailButton;
+    private JScrollPane selectionScrollable;
+    private JButton exitButton;
+}
