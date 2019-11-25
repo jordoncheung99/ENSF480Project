ada6254792e8b22cf32f6e23be3cc777e2898813
rnward0 <49202674+rnward0@users.noreply.github.com>
2019-11-25 09:41:42 -0700

    Criteria GUI

---
+++ src/GUI/CriteriaGUI.java
@@ -0,0 +1,16 @@
+package GUI;
+
+import javax.swing.*;
+
+public class CriteriaGUI {
+    private JLabel furnishedEnableLabel;
+    private JLabel furnishedLabel;
+    private JLabel numberOfBathRoomsLabel;
+    private JLabel numberOfBedRoomsLabel;
+    private JLabel typeOfPropertyLabel;
+    private JLabel areaLabel;
+    private JLabel rentTermLabel;
+    private JLabel rentAmountLabel;
+    private JLabel cityQuadrantLabel;
+    private JLabel propertyIDLabel;
+}
