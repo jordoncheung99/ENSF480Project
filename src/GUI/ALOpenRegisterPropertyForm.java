0f7fd152dd3b0f0e0f73b5fbb4f20ad4d54d183e
jordoncheung99 <jordoncheung99@gmail.com>
2019-11-24 17:23:44 -0700

    Registation form GUI made

---
+++ src/GUI/ALOpenRegisterPropertyForm.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ALOpenRegisterPropertyForm implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+//        JFrame frame = new JFrame("RegisterForm");
+//        frame.setContentPane(new LandlordRegisterPropertyGUI().panel1);
+//        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+//        frame.pack();
+//        System.out.println("here?");
+//        registerButton.addActionListener(new LandlordRegisterPropertyGUI.ALRegister());
+//
+//
+//        frame.setVisible(true);
+//        System.out.println("vislbe");
+        LandlordRegisterPropertyGUI regProp = new LandlordRegisterPropertyGUI();
+    }
+}
--- src/GUI/searchProperty.java
+++ src/GUI/searchProperty.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
